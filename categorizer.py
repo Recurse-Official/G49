bbe9cc0c52bceb71f4933f616e45af1d14f8b04b
heeehe <tejaswi170204@gmail.com>
2024-11-30 19:58:44 +0530

    update categorizer

--- categorizer.py
+++ categorizer.py
@@ -1,32 +1,35 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch
+import json
 
-# Load the pretrained model and tokenizer
 MODEL_NAME = "moctarsmal/bank-transactions-statements-classification"
 
-# Initialize tokenizer with explicitly defined special tokens
-tokenizer = AutoTokenizer.from_pretrained(
-    MODEL_NAME,
-    use_fast=False,
-    bos_token="<s>",
-    eos_token="</s>",
-    pad_token="<pad>",
-    unk_token="<unk>"
-)
+# Correct and reload the tokenizer
+tokenizer_path = "C:/Users/K TEJASWI/Downloads/vocab.json"
 
-# Add additional special tokens explicitly (if needed)
-additional_special_tokens = ["<special0>", "<special1>", "<special2>", "<special3>", "<special4>"]
-tokenizer.add_special_tokens({"additional_special_tokens": additional_special_tokens})
+with open(tokenizer_path, 'r') as f:
+    corrected_vocab = json.load(f)
 
-# Load the model and resize embeddings to accommodate the added tokens
+# Define special tokens explicitly
+special_tokens = {
+    "bos_token": "<s>",
+    "eos_token": "</s>",
+    "pad_token": "<pad>",
+    "unk_token": "<unk>",
+    "additional_special_tokens": ["<special0>", "<special1>", "<special2>", "<special3>", "<special4>"]
+}
+
+# Load tokenizer with corrected vocab
+tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, use_fast=False)
+tokenizer.add_tokens(list(corrected_vocab.keys()))
+tokenizer.add_special_tokens(special_tokens)
+
+# Resize the model's embeddings to match the updated tokenizer
 model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME)
 model.resize_token_embeddings(len(tokenizer))
 
 # Mapping model output to categories
-LABELS = {
-    0: "Utility",
-    1: "Non-Utility"
-}
+LABELS = {0: "Utility", 1: "Non-Utility"}
 
 def categorize_transaction(description):
     """
@@ -42,6 +45,7 @@ def categorize_transaction(description):
 
     # Map the predicted label to its corresponding category
     return LABELS[predicted_label]
+
 
 
 
