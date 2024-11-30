f2e35bc9bd1cff80f1b532d6d857c9e962c49868
sanju1u82 <sanjusrir22@gmail.com>
2024-12-01 04:59:04 +0530

    react

--- frontend1/src/components/Login.js
+++ frontend1/src/components/Login.js
@@ -2,19 +2,19 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { setToken } from '../utils/auth';
-import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    const history = useHistory();
+    const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
             const res = await axios.post('http://localhost:5000/auth/login', { username, password });
             setToken(res.data.access_token);
-            history.push('/dashboard');
+            navigate("/dashboard");
         } catch (err) {
             console.error(err);
             alert('Login failed');
--- frontend1/src/components/Register.js
+++ frontend1/src/components/Register.js
@@ -1,19 +1,19 @@
 // frontend/src/components/Register.js
 import React, { useState } from 'react';
 import axios from 'axios';
-import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Register = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    const history = useHistory();
+    const navigate = useNavigate();
 
     const handleRegister = async (e) => {
         e.preventDefault();
         try {
             const res = await axios.post('http://localhost:5000/auth/register', { username, password });
             alert('Registration successful. Please log in.');
-            history.push('/login');
+            navigate("/login");
         } catch (err) {
             console.error(err);
             alert('Registration failed');
