802facfdcc342a5926141e44cf3a6387f8abf0c4
sanju1u82 <sanjusrir22@gmail.com>
2024-12-01 04:58:03 +0530

    auth

--- frontend1/src/utils/auth.js
+++ frontend1/src/utils/auth.js
@@ -1,5 +1,5 @@
 // frontend/src/utils/auth.js
-import jwtDecode from 'jwt-decode';
+import { jwtDecode } from 'jwt-decode';
 
 export const setToken = (token) => {
     localStorage.setItem('token', token);
